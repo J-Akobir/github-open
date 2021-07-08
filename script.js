930ae522f763720a0747e4ffa27f864baffacd5d
Akobir <jumayevakobir777@gmail.com>
2021-07-08 13:51:18 +0300

    new project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let number = 4;
+/*let number = 4;
 
 console.log(number);
 
@@ -10,3 +10,22 @@ console.log(a);
 
 const userAge = confirm("Yoshiz 18 dami ");
 console.log(userAge);
+*/
+"use strict";
+
+const numberOfNews = +prompt("Siz qancha ko'rdingiz");
+
+const personalDB = {
+  count: numberOfNews,
+  actors: {},
+  news: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Oxirgi yangilik"),
+  b = prompt("Unga baho bering");
+
+personalDB.news[a] = b;
+personalDB.actors[b] = a;
+console.log(personalDB);
