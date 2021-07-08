81713fa52c48c26b4e8acc470464d9396a0f4bd0
Akobir <jumayevakobir777@gmail.com>
2021-07-08 09:48:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let number = 4;
+
+console.log(number);
+
+number = 12;
+console.log(number);
+
+let a = 1 + "1" - 1;
+console.log(a);
+
+const userAge = confirm("Yoshiz 18 dami ");
+console.log(userAge);
