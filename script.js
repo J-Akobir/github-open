52abd0b41cdaa188010ea4a9448617682cf6883d
Akobir <jumayevakobir777@gmail.com>
2021-07-14 23:24:15 +0300

    function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*let number = 4;
 
 console.log(number);
@@ -11,21 +13,91 @@ console.log(a);
 const userAge = confirm("Yoshiz 18 dami ");
 console.log(userAge);
 */
-"use strict";
 
-const numberOfNews = +prompt("Siz qancha ko'rdingiz");
+// const numberOfNews = +prompt("Siz qancha ko'rdingiz");
 
-const personalDB = {
-  count: numberOfNews,
-  actors: {},
-  news: {},
-  genres: [],
-  privat: false,
-};
+// const personalDB = {
+//   count: numberOfNews,
+//   actors: {},
+//   news: {},
+//   genres: [],
+//   privat: false,
+// };
 
-const a = prompt("Oxirgi yangilik"),
-  b = prompt("Unga baho bering");
+// const a = prompt("Oxirgi yangilik"),
+//   b = prompt("Unga baho bering");
 
-personalDB.news[a] = b;
-personalDB.actors[b] = a;
-console.log(personalDB);
+// personalDB.news[a] = b;
+// personalDB.actors[b] = a;
+// console.log(personalDB);
+
+// const array = [1, 2, 3, 4, 5];
+// let sum = 0,
+//   temp = 0;
+// for (let i = 0; i < array.length; i++) {
+//   sum += array[i];
+// }
+// temp = sum / (array.length-1);
+// console.log(sum);
+// console.log(temp);
+
+// const array1 = [1, 2, 3, 4, 5, 6];
+// const reducer = (a, b) => a + b;
+
+// 1 + 2 + 3 + 4
+// console.log(array1.reduce(reducer));
+
+// //Massiv indekslari toq qiymatlari
+// const array = [1, 2, 3, 4, 5, 11, 7, 20];
+// let sum = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//   if (i % 2 == 0) {
+//     sum = array[i];
+//     console.log(sum);
+//   }
+// }
+
+//Massiv toq o'rindagilar qiymatlari//
+// const array2 = [1, 2, 3, 4, 5, 11, 7, 20];
+// let sum2 = 0;
+
+// for(let i = 0; i < array2.length; i++){
+//   if(array2[i] % 2 == 1){
+//     sum2 = array2[i];
+//     console.log(sum2);
+//   }
+// }
+
+// let number = 8;
+
+// function getNumber(text) {
+//   console.log(text);
+//   number = 10;
+// }
+// getNumber("hoool");
+// console.log(number);
+
+// function calc(a, b) {
+//   return a+b;
+
+// }
+// console.log(calc(3, 14));
+
+// function playcall(football, callback) {
+//   console.log(`hi ${football}`);
+//   callback();
+// }
+
+// playcall("moSalah", function () {
+//   console.log("hi 1001 goals");
+// });
+
+// const text = "HELLo world";
+// console.log(text.toUpperCase());
+// console.log(text.indexOf("o"));
+// console.log(text.slice(6, 9));
+
+const number = 7.7;
+
+console.log(Math.round(number));
